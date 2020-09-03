1b95a23427858336d8b97c4fd6b80617358a54d2
Noam Lin <noamlin@gmail.com>
2020-09-03 16:49:08 +0300

    more path updates

--- app/index.js
+++ app/index.js
@@ -9,15 +9,15 @@ const nunjucksEnv = require('./nunjucks-env.js');
 nunjucksEnv.express(app);
 
 app.use('/public', express.static(`${baseDir}/public/`));
-app.get('/', (req, res) => { res.render('homepage.njk'); });
+app.get('/', (req, res) => { res.render('index.njk'); });
 app.get('/projects', (req, res) => { res.render('projects.njk'); });
 app.get('/contact', (req, res) => { res.render('contact.njk'); });
 
-app.get('/oh', (req, res) => { res.render('oh/getting-started.njk'); });
+app.get('/oh', (req, res) => { res.render('oh/index.njk'); });
 app.get('/oh/api', (req, res) => { res.render('oh/api.njk'); });
 app.get('/oh/demos', (req, res) => { res.render('oh/demos.njk'); });
 
-app.get('/proxserve', (req, res) => { res.render('proxserve/getting-started.njk'); });
+app.get('/proxserve', (req, res) => { res.render('proxserve/index.njk'); });
 app.get('/proxserve/api', (req, res) => { res.render('proxserve/api.njk'); });
 app.get('/proxserve/demos', (req, res) => { res.render('proxserve/demos.njk'); });
 
